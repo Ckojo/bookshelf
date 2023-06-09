c3de58a5833311e5e7b346eb897ab1d67369fc53
Jovan Sremacki <jovansremacki@gmail.com>
2023-06-09 23:55:52 +0200

    Initial code structure

--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Bookshelf
-  class Routes < Hanami::Routes
-    root to: "home.show"
-    get "/home/:id", to: "home.show"
-    get "/books", to: "books.index"
-  end
-end
