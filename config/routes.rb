0eee522d8f78f71ab865c3674932ae796d3bf696
Jovan <jovansr@protonmail.com>
2023-06-09 23:46:39 +0200

    Inital commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Bookshelf
+  class Routes < Hanami::Routes
+    root to: "home.show"
+    get "/home/:id", to: "home.show"
+    get "/books", to: "books.index"
+  end
+end
